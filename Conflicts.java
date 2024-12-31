f3bc0d6711cbe14f7fb169dfb3b308c940b8f1a4
krishuuu016 <rajputkrishuuu@gmail.com>
2024-12-31 10:26:43 +0530

    added conflict.java file

---
+++ Conflicts.java
@@ -0,0 +1,10 @@
+public class Conflicts {
+    public static void main(String[] args) {
+        String name = "Divyansh Rajput";
+        int roll = 43;
+
+        int c = 40;
+
+        System.out.println(age);
+    }
+}
