82bebd8ed764a0edf28c007b5395e75bfd0a39f5
krishuuu016 <rajputkrishuuu@gmail.com>
2024-12-31 13:23:48 +0530

    bug fixed

--- cherrypick.java
+++ cherrypick.java
@@ -1,6 +1,6 @@
 public class cherrypick {
     public static void main(String[] args) {
-        helloworld("system.out.println"); // this is the bug which has been 
+        System.out.println("hello world");; // this is the bug which has been 
                                           // fixed by other devloper in other branch
     }
 }
