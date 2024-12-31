d86697460ea33e221ac09a79ed623ba8f20dc99c
krishuuu016 <rajputkrishuuu@gmail.com>
2024-12-31 13:21:51 +0530

    first commit

---
+++ cherrypick.java
@@ -0,0 +1,6 @@
+public class cherrypick {
+    public static void main(String[] args) {
+        helloworld("system.out.println"); // this is the bug which has been 
+                                          // fixed by other devloper in other branch
+    }
+}
